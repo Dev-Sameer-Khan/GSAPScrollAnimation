38c814d97dda2de5c7bb028fcccc341811c76ab1
LazyCodwr <khansameer84233@gmail.com>
2025-01-19 23:11:08 +0530

    Enhance index.html and script.js with new features and styles
    - Added meta tags for character encoding and responsive design in index.html.
    - Included Tailwind CSS via CDN for styling.
    - Implemented custom font imports and improved navigation bar with a star SVG logo.
    - Enhanced the hero section with a full-screen image and text overlay.
    - Introduced hover effects for the menu button using GSAP animations.
    - Updated JavaScript in script.js to include new animations and scroll triggers for dynamic effects.

--- script.js
+++ script.js
@@ -1,12 +1,104 @@
-gsap.from(".hero img", {
-   scale: 0.08,
-   borderRadius: "35px",
+let page4 = document.querySelector("#page4")
+let page5 = document.querySelector("#page5")
+
+
+   // let tl = gsap.timeline({
+   //    scrollTrigger: {
+   //       trigger : ".hero",
+   //       start : "top top",
+   //       end : "bottom top",
+   //       scrub : 2,
+   //       pin : true,
+   //       ease : "power4",
+   //      }
+   // })
+
+// tl.from(".hero img", {
+//    // scale: 0.08,
+//    borderRadius: "40px",
+//    duration : 4
+// })
+// .to(".hero img", {
+//    // scale: 0.9,
+//    borderRadius: "40px",
+//    duration : 4
+// },"b")
+
+// tl.to(".hero .txt", {
+//    display : "none",
+//    duration : 2
+// },"b")
+// .to(".hero img",{
+//    rotateX : "-90deg",
+//    transformOrigin : "top",
+//    duration : 4
+// },"a")
+
+let tl = gsap.timeline({
    scrollTrigger: {
-    trigger : ".hero",
-    start : "top top",
-    end : "bottom top",
-    scrub : 2,
-    pin : true,
-    ease : "power4"
+      trigger: ".hero",
+      start: "top top", 
+      end: "bottom top",
+      scrub: 2,
+ 
    }
 })
+
+tl.from(".hero img", {
+   scale: 0.08,
+   borderRadius: "40px",
+   duration: 2
+})
+.to(".hero",{
+   scale : 0.9,
+   borderRadius: "40px",
+   duration: 2
+})
+.to("main",{
+   backgroundColor : "yellow"
+},"a")
+.to(".hero",{
+   rotateX : "-60deg",
+   transformOrigin : "top",
+   duration: 2
+})
+
+
+document.querySelector("#hover").addEventListener("mouseenter", () => {
+   gsap.to("#hide", { 
+      x: 0,
+      opacity : 1,
+      duration : 0.5,
+      ease : "power4"
+   });
+   gsap.to("#up2",{
+      y: -24,
+      ease : "power4",
+      delay : 0.1
+    });
+    gsap.to("#up1",{
+      y: -48,
+      ease : "power4",
+      delay : 0.1
+    });
+});
+document.querySelector("#hover").addEventListener("mouseleave", () => {
+   gsap.to("#hide", { 
+      x: 20,
+      opacity : 0,
+      duration : 0.5,
+      ease : "power4"
+   });
+   gsap.to("#up2",{
+      y: 0,
+      ease : "power4"
+    });
+    gsap.to("#up1",{
+      y: 0,
+      ease : "power4"
+    });
+});
+
+
+
+
